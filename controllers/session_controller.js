e821b9b6f3e2e11b9d5b75ccf1b1292f95d435c7
powlam <palbandoz+github@gmail.com>
2015-08-04 19:53:56 +0200

    Necesaria autorización para la edición de preguntas

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,5 +1,14 @@
 var models = require('../models/models.js');
 
+//MW de autorización
+exports.loginRequired = function(req, res, next) {
+	if (req.session.user) {
+		next();
+	} else {
+		res.redirect('/login');
+	}
+};
+
 // GET /login
 exports.new = function(req, res) {
 	var errors = req.session.errors || {};
--- routes/index.js
+++ routes/index.js
@@ -15,14 +15,15 @@ router.get('/', function(req, res) {
 
 router.param('quizId', quizController.load); //Autoload si existe :quizId
 
-router.get('/quizes', quizController.index);
-router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/quizes/new', quizController.new);
-router.post('/quizes/create', quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
-router.put('/quizes/:quizId(\\d+)', quizController.update);
-router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+//Las siguientes solo se pasan a su controlador si el MW loginRequired lanza next()
+router.get('/quizes/new',                sessionController.loginRequired, quizController.new);
+router.post('/quizes/create',            sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)',      sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)',   sessionController.loginRequired, quizController.destroy);
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
